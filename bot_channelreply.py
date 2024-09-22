28101311cb64fbe648e69299bc52a1fd20432cc5
Olivia <98853542+oliviamrkw@users.noreply.github.com>
2024-09-22 15:56:37 -0400

    Update bot_channelreply.py

--- bot_channelreply.py
+++ bot_channelreply.py
@@ -2,29 +2,15 @@ import discord
 import asyncio
 import random
 
-##msgs_list = [
-##    'rabbit1.png',
-##    'rabbit2.png',
-##    'rabbit3.png',
-##    'rabbit4.png',
-##    'rabbit5.png',
-##    'rabbit6.png',
-##    'rabbit7.png',
-##    'rabbit8.png',
-##    'rabbit9.png',
-##    'rabbit10.png',
-##    ]
-
 # Set up the bot client with intents
 intents = discord.Intents.default()
 intents.message_content = True  # This intent is needed to read message content
 
 client = discord.Client(intents=intents)
 
-# Replace with your bot's token
 TOKEN = 'YOUR TOKEN'
     
-CHANNEL_ID = 1273657075561402380
+CHANNEL_ID = 905143091504226354
 
 @client.event
 async def on_ready():
@@ -35,17 +21,28 @@ async def on_message(message):
     # Ignore messages from the bot itself
     if message.author == client.user:
         return
+
+    msg = None
+    image = None
     
     # Check if the message content is 'rabbit' 
     if 'rabbit' in message.content.lower() or 'bunny' in message.content.lower() or '🐰' in message.content or '🐇' in message.content:
-        number = random.randint(1,21)
+        number = random.randint(1,63)
         # Choose a random image from the list
         image = 'rabbit'+str(number)+'.png'
+        msg = "rabbit :3"
         
         # Send the image to the channel where the message was received
+
+    elif 'hare' in message.content.lower():
+        number = random.randint(1,8)
+        image = 'hare'+str(number)+'.jpg'
+        msg = "hare :3"
+
+    if msg and image:
         channel = message.channel
         try:
-            await channel.send("rabbit :3", file=discord.File(image))
+            await channel.send(msg, file=discord.File(image))
             print(f"Sent: {image}")
         except discord.errors.Forbidden:
             print("Cannot send messages to the channel due to permissions or other restrictions.")
